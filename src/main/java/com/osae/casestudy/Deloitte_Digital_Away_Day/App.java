cf439026d94742ea13ccded01391f22bdc9118e3
Daniel Amoako Osae <daniel@daniels-mbp.mynet>
2018-05-30 23:36:33 +0100

    Initial commit

---
+++ src/main/java/com/osae/casestudy/Deloitte_Digital_Away_Day/App.java
@@ -0,0 +1,13 @@
+package com.osae.casestudy.Deloitte_Digital_Away_Day;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
