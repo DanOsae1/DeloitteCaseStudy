ae9f92895a4af359c2ebbee1063451ad3c0ec5cf
kobbiedan <35192598+kobbiedan@users.noreply.github.com>
2018-06-04 15:10:16 +0100

    Update AppTest.java

--- src/test/java/com/osae/casestudy/Deloitte_Digital_Away_Day/AppTest.java
+++ src/test/java/com/osae/casestudy/Deloitte_Digital_Away_Day/AppTest.java
@@ -1,5 +1,8 @@
 package com.osae.casestudy.Deloitte_Digital_Away_Day;
 
+import com.osae.casestudy.RWManager.test.RWManagerTest;
+import com.osae.casestudy.fileManager.RWManager;
+import com.osae.casestudy.timetable.test.TimetableTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -8,12 +11,11 @@ import com.osae.casestudy.activity.test.ActivitiesTest;
 import com.osae.casestudy.activity.test.EventTest;
 import com.osae.casestudy.print.test.PrintTest;
 import com.osae.casestudy.team.test.TeamTest;
-import com.osae.casestudy.timetable.Timetable;
 
 import junit.framework.TestCase;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PrintTest.class, TeamTest.class, ActivitiesTest.class, EventTest.class, Timetable.class })
+@SuiteClasses({PrintTest.class, RWManagerTest.class, TeamTest.class, ActivitiesTest.class, EventTest.class, TimetableTests.class})
 public class AppTest extends TestCase {
 
 }
