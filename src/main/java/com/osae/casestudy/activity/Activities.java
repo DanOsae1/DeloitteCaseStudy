156d11554c19368490119aff459429b5a7022eee
kobbiedan <35192598+kobbiedan@users.noreply.github.com>
2018-06-04 15:07:16 +0100

    Update Activities.java

--- src/main/java/com/osae/casestudy/activity/Activities.java
+++ src/main/java/com/osae/casestudy/activity/Activities.java
@@ -1,47 +1,54 @@
 package com.osae.casestudy.activity;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
 import com.osae.casestudy.App;
+import com.osae.casestudy.fileManager.RWManager;
 
 public class Activities {
 
-	public static String filePath = "/activities.txt";
-	private final static Logger log = Logger.getLogger(Activities.class.getName());
 
-	public List<Event> getActivitesList() throws IOException {
-		return getActivitiesList(filePath);
-	}
+    private final static Logger log = Logger.getLogger(Activities.class.getName());
 
-	private List<Event> getActivitiesList(String filePath) throws IOException {
-		List<Event> activitiesList = new ArrayList<>();
-		log.info("Reading activities list file");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(Activities.class.getResourceAsStream(filePath)));
-		Event event;
-		String activities;
-		while ((activities = reader.readLine()) != null) {
-			if (activities.contains("sprint")) {
-				event = new Event(15, activities);
-				activitiesList.add(event);
-			} else {
-				String[] key = activities.split(" ");
-				for (int i = 0; i < key.length; i++) {
-					if (key[i].matches("([0-9])\\d+(min)")) {
-						int mins = Integer.parseInt((String) key[i].subSequence(0, 2));
-						event = new Event(mins, activities);
-						activitiesList.add(event);
-					}
-				}
+    public List<Event> getActivitesList() throws IOException {
+        return getActivitiesList();
+    }
 
-			}
+    private List<Event> getActivitiesList() {
+        try {
+            List<Event> activitiesList = new ArrayList<>();
+            log.info("Reading activities list file");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(RWManager.getActivitiesFile()));
+            Event event;
+            String activities;
+            while ((activities = reader.readLine()) != null) {
+                if (activities.contains("sprint")) {
+                    event = new Event(15, activities);
+                    activitiesList.add(event);
+                } else {
+                    String[] key = activities.split(" ");
+                    for (int i = 0; i < key.length; i++) {
+                        if (key[i].matches("([0-9])\\d+(min)")) {
+                            int mins = Integer.parseInt((String) key[i].subSequence(0, 2));
+                            event = new Event(mins, activities);
+                            activitiesList.add(event);
+                        }
+                    }
 
-		}
-		return activitiesList;
-	}
+                }
 
+            }
+            return activitiesList;
+        } catch (IOException e) {
+            log.severe(e.getCause().getMessage());
+        }
+        return Collections.emptyList();
+    }
 }
